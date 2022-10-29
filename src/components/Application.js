a0fd20c8419e25d3f9a84694ba8c80efab5d80fc
Sara <parakeet_22@yahoo.com>
2022-10-28 21:38:17 -0400

    mostly works but 5pm endpoint has dissappeared with blank appointment data

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,45 +8,45 @@ import Appointment from 'components/Appointment';
 
 
 
-//Mock Data Appointments
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+// //Mock Data Appointments
+// const appointments = {
+//   "1": {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   "2": {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer:{
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   "3": {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   "4": {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer:{
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   "5": {
+//     id: 5,
+//     time: "4pm",
+//   }
+// };
 
 
 
@@ -54,10 +54,21 @@ const appointments = {
 //Application
 
 export default function Application(props) {
-//useState variables
-const [day, setDay] = useState('Monday');
-//Days State to replace static data
-const [days, setDays] = useState([]);
+//all of the data in one state
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+//Daily Appoinments - hold appointments for specific day
+const dailyAppointments = [];
+
+  //setDay function
+const setDay = day => setState({ ...state, day });
+const setDays = (days) => {
+  setState(prev => ({ ...prev, days }));
+}
+
 //Get days from the server
 
 useEffect(() => {
@@ -75,7 +86,7 @@ useEffect(() => {
 
 
 //Convert Data Object to Array of appointments
-const appointmentsArray = Object.values(appointments).map(appointment => {
+const appointmentsArray = dailyAppointments.map(appointment => {
   return (
     <Appointment
     key={appointment.id}
@@ -85,7 +96,7 @@ const appointmentsArray = Object.values(appointments).map(appointment => {
   )
 });
 
-console.log(day);
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -97,8 +108,8 @@ console.log(day);
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu">
 <DayList
-  days={days}
-  value={day}
+  days={state.days}
+  value={state.day}
   onChange={setDay}
 />
 </nav>
