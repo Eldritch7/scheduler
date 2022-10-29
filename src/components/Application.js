10167a954d1919b169c296f6d2019b618f06cf3e
Sara <parakeet_22@yahoo.com>
2022-10-28 22:18:41 -0400

    server appointments working! still need to cleanup

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from "react";
-import Axios from '../../node_modules/axios/index';
+import axios from '../../node_modules/axios/index';
 import "components/Application.scss";
 
 //Import Components
 import DayList from 'components/DayList';
 import Appointment from 'components/Appointment';
+
+//Helper Functions
+import { getAppointmentsForDay } from "helpers/selectors";
 
 
 
@@ -61,7 +64,7 @@ export default function Application(props) {
     appointments: {}
   });
 //Daily Appoinments - hold appointments for specific day
-const dailyAppointments = [];
+const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   //setDay function
 const setDay = day => setState({ ...state, day });
@@ -73,12 +76,31 @@ const setDays = (days) => {
 
 useEffect(() => {
   const daysUrl =  `http://localhost:8001/api/days`;
-  Axios.get(daysUrl).then(response => {
+  const appointmentsUrl = `http://localhost:8001/api/appointments`;
+  const interviewersUrl = `http://localhost:8001/api/interviewers`;
 
-    console.log(`response:`, response);
-    setDays([...response.data]);
-    console.log(`response.data in days:`, response.data);
-  })
+  Promise.all([
+    axios.get(daysUrl),
+    axios.get(appointmentsUrl),
+    axios.get(interviewersUrl)
+  ]).then((all) => {
+    setState(prev => ({...prev, 
+      days: all[0].data,
+      appointments: all[1].data,
+      //interviewers: all[2].data 
+    }));
+    console.log(all)
+    console.log(all[0]); // first
+    console.log(all[1]); // second
+    console.log(all[2]); // third
+  });
+
+  // Axios.get(daysUrl).then(response => {
+
+  //   console.log(`response:`, response);
+  //   setDays([...response.data]);
+  //   console.log(`response.data in days:`, response.data);
+  // })
 
 }, []);
 
