70c137652b0349726f9e332bfb9a9c5e7e93fba2
Sara <parakeet_22@yahoo.com>
2022-11-07 08:03:54 -0500

    added basic use of custom hook with empty, show and cancel - some old code still in there

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,19 +3,52 @@ import "components/Appointment/styles.scss";
 import Empty from "components/Appointment/Empty";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
+import Form from "components/Appointment/Form";
+import Status from "components/Appointment/Status";
+import Confirm from "components/Appointment/Confirm";
+import Error from "components/Appointment/Error";
+
+
+import useVisualMode from "hooks/useVisualMode";
+
+
 
 
 export default function Appointment(props) {
+  //Modes
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
+//Passing the mode to visual Mode
+const { mode, transition, back } = useVisualMode(
+  props.interview ? SHOW : EMPTY
+);
+  //returning components
   return (
+    
 <article className="appointment">
 <Header
 time={props.time}
 />
-{props.interview ? <Show 
+{mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+{mode === SHOW && (
+  <Show
+    student={props.interview.student}
+    interviewer={props.interview.interviewer}
+  />
+)}
+{mode === CREATE && (
+        <Form 
+          interviewers={[]}
+          onCancel = {back}
+        />
+      )}
+{/* {props.interview ? <Show 
 student = {props.interview.student}
 interviewer = {props.interview.interviewer}
 /> : 
-<Empty />}
+<Empty />} */}
 
 
 
